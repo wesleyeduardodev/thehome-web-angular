b72f2aebf02483dd5e21def9025d6971136ce968
wesleyeduardo.dev <111645247+wesleyeduardodev@users.noreply.github.com>
2023-10-17 11:30:37 -0300

    inserindo listagem de clientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {NavComponent} from "./components/nav/nav.component";
+import {ClienteListComponent} from "./components/cliente/cliente-list/cliente-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: NavComponent, children: [
+      {path: 'home', component: HomeComponent},
+      {path: 'clientes', component: ClienteListComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/models/client.ts
@@ -0,0 +1,9 @@
+export interface Client {
+  id?: any;
+  name?: string;
+  cpf?: string;
+  cnpj?: string;
+  telephone?: string;
+  email?: string;
+}
+
