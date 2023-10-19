11bb60323509687eb8f719cb4b7278f940c99007
wesleyeduardo.dev <wesleyeduardo.dev@gmail.com>
2023-10-19 13:56:31 -0300

    ajuste rota

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,6 +1,5 @@
-export const PROD_API_CONFIG = {
-  baseUrl: 'https://thehome-c09b93f0e0dc.herokuapp.com/api'
-}
-export const LOCAL_API_CONFIG = {
-  baseUrl: 'http://localhost:8080/api'
+const URL_PROD = 'https://thehome-c09b93f0e0dc.herokuapp.com/api';
+const URL_LOCAL = 'http://localhost:8080/api';
+export const API_CONFIG = {
+  baseUrl: URL_LOCAL
 }
