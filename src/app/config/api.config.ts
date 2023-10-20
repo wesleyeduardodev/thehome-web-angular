d953ccf3ea44ebee6850da26f81aec26f0740388
wesleyeduardo.dev <wesleyeduardo.dev@gmail.com>
2023-10-19 21:28:32 -0300

    ajust base url

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,5 +1,5 @@
-const URL_PROD = 'https://thehome-c09b93f0e0dc.herokuapp.com/api';
+const URL_PROD = 'https://thehome-3a98079ba9ed.herokuapp.com/api';
 const URL_LOCAL = 'http://localhost:8080/api';
 export const API_CONFIG = {
-  baseUrl: URL_LOCAL
+  baseUrl: URL_PROD
 }
